b7b3715777f37de2607fcc97f6786dbcd5ef38d0
Cebo Jordaan <cjordaan@student.wethinkcode.co.za>
2019-04-15 13:58:17 +0200

    prctising

---
+++ Documents/GitHub/dogs.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dogs.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjordaan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/15 13:54:49 by cjordaan          #+#    #+#             */
+/*   Updated: 2019/04/15 13:55:10 by cjordaan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+//int double float
+int dogs = 6;
+
+//floating point
+//precision -- how much data the variable can hold
+
+printf("%i %f %f\n", 1, 1.1111, 1.1111F);
+
+return(0);
+}
