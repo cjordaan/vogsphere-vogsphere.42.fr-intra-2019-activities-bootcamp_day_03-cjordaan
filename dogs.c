6bfef8cf8af4cae6e67f8ad99df1fd897b3360f4
Cebo Jordaan <cjordaan@student.wethinkcode.co.za>
2019-04-16 12:57:32 +0200

    dogs

---
+++ dogs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int dogs = 2.5e4;
+
+    //floating point
+    //prcision -- how much data the variable can hold
+
+    print("%i %f %f\n". 1. 1111.1111 1.1111F);
+
+    return(0);
+}
