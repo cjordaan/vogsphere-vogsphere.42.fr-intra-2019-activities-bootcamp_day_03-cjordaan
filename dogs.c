ce0698fa33efc5cd5bd49ddc6c95d8c056844166
Cebo Jordaan <cjordaan@student.wethinkcode.co.za>
2019-04-16 13:00:18 +0200

    dogs

--- dogs.c
+++ dogs.c
@@ -3,12 +3,7 @@
 
 int main()
 {
-    int dogs = 2.5e4;
-
-    //floating point
-    //prcision -- how much data the variable can hold
-
-    print("%i %f %f\n". 1. 1111.1111 1.1111F);
+    printf("hellow");
 
     return(0);
 }
